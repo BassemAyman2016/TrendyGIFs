ea00ef163a9e6d46c22350b0343883633a0b0fa7
Bassem <bassem.ayman1997@gmail.com>
2021-06-29 06:14:21 +0200

    init project

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
 
+const vuexLocal = new VuexPersistence({
+  key: "basicData",
+  storage: window.localStorage,
+});
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -8,4 +13,5 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
